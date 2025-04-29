b104022e77fb5384eee80f1102414b1fc3a18729
Artem <artem.chebanyuk@gmail.com@email.com>
2025-04-29 19:39:38 +0300

    user_fields

--- backend_api/app/applications/users/schemas.py
+++ backend_api/app/applications/users/schemas.py
@@ -1,5 +1,11 @@
 from pydantic import BaseModel, EmailStr, Field
 
-class UserFields(BaseModel):
+class BaseFields(BaseModel):
     email: EmailStr = Field(description='User email', examples=['artem.chebanyuk@gmail.com'])
     name: str = Field(description='User nickname', example=['Casper'])
+
+class PasswordField(BaseModel):
+    password: str = Field(min_lenght=8)
+
+class RegisterUserFields(BaseFields, PasswordField):
+    pass
