0c0fe24834e867dcd520fd0d641598f2f519fce6
Artem <artem.chebanyuk@gmail.com@email.com>
2025-05-09 19:44:15 +0300

    insert users in db

--- backend_api/app/applications/users/router.py
+++ backend_api/app/applications/users/router.py
@@ -1,10 +1,10 @@
 from fastapi import APIRouter, status
-
+from sqlalchemy.ext.asyncio import AsyncSession
 from applications.users.schemas import BaseFields, RegisterUserFields
 
 router_users = APIRouter()
 
 
 @router_users.post("/create", status_code=status.HTTP_201_CREATED)
-async def creat_user(new_user: RegisterUserFields) -> BaseFields:
+async def creat_user(new_user: RegisterUserFields, session: AsyncSession) -> BaseFields:
     return new_user
