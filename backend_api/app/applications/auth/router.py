f1dda170421f0947c4c716e80accbd1395204e1d
Artem <artem.chebanyuk@gmail.com@email.com>
2025-05-16 19:28:27 +0300

    Auth

--- backend_api/app/applications/auth/router.py
+++ backend_api/app/applications/auth/router.py
@@ -1,9 +1,9 @@
-from fastapi import APIRouter
-
+from fastapi import APIRouter, Depends
+from fastapi.security import OAuth2PasswordRequestForm
 
 router_auth = APIRouter()
 
 
-@router_auth.post('/login')
-async  def user_login():
+@router_auth.post("/login")
+async def user_login(data: OAuth2PasswordRequestForm = Depends()):
     return
