8ad5fee72e3cd9595b62dd8d7f6b06d42c06f97b
Artem <artem.chebanyuk@gmail.com@email.com>
2025-05-27 19:42:57 +0300

    templates

--- frontend/app/routers/main_page_router.py
+++ frontend/app/routers/main_page_router.py
@@ -1,8 +1,13 @@
-from fastapi import APIRouter
-
+from fastapi import APIRouter, Request
+from fastapi.templating import Jinja2Templates
 
 router = APIRouter()
+templates = Jinja2Templates(directory='templates')
+
 
 @router.get('/')
-async def index():
-    return 'hyifahwgdiywh'
+async def index(request: Request):
+    context = {'request': request, }
+    response = templates.TemplateResponse('index.html', context=context)
+    return response
+
