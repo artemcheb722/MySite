3b5dcd93e569869ed162c3bfc471ff5a4cf95d0d
Artem <artem.chebanyuk@gmail.com@email.com>
2025-06-10 20:13:13 +0300

    settings

--- backend_api/app/applications/users/router.py
+++ backend_api/app/applications/users/router.py
@@ -6,6 +6,8 @@ from sqlalchemy.ext.asyncio import AsyncSession
 from applications.users.crud import activate_user_account, create_user_in_db, get_user_by_email
 from applications.users.schemas import BaseUserInfo, RegisterUserFields
 from database.session_dependencies import get_async_session
+from services.rabbit.constans import SupportedQueues
+from services.rabbit.rabbitmq_servise import rabbitmq_broker
 
 router_users = APIRouter()
 
@@ -17,6 +19,7 @@ async def creat_user(new_user: RegisterUserFields, session: AsyncSession = Depen
         raise HTTPException(status_code=status.HTTP_409_CONFLICT, detail="Already exists")
 
     created_user = await create_user_in_db(new_user.email, new_user.name, new_user.password, session)
+    await rabbitmq_broker.send_message({"ss": "bb"}, SupportedQueues.USER_REGISTRATION)
     return created_user
 
 
--- backend_api/app/settings.py
+++ backend_api/app/settings.py
@@ -15,6 +15,12 @@ class Settings(BaseSettings):
     JWT_ALGORITHM: str
     SENTRY: str
 
+    RMQ_HOST: str
+    RMQ_PORT: int
+    RMQ_VIRTUAL_HOST: str
+    RMQ_USER: str
+    RMQ_PASSWORD : str
+
     @property
     def DATABASE_URL_ASYNC(self) -> str:
         return (
