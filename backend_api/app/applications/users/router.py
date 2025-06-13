f2cb70e304aea85e2def63fa468ffa1060421e02
Artem <artem.chebanyuk@gmail.com@email.com>
2025-06-13 19:27:14 +0300

    settings

--- backend_api/app/applications/users/router.py
+++ backend_api/app/applications/users/router.py
@@ -1,6 +1,6 @@
 import uuid
 
-from fastapi import APIRouter, Depends, HTTPException, status
+from fastapi import APIRouter, Depends, HTTPException, status, Request, BackgroundTasks
 from sqlalchemy.ext.asyncio import AsyncSession
 
 from applications.users.crud import activate_user_account, create_user_in_db, get_user_by_email
@@ -13,13 +13,27 @@ router_users = APIRouter()
 
 
 @router_users.post("/create", status_code=status.HTTP_201_CREATED)
-async def creat_user(new_user: RegisterUserFields, session: AsyncSession = Depends(get_async_session)) -> BaseUserInfo:
+async def create_user(
+        request: Request,
+        new_user: RegisterUserFields,
+        background_task: BackgroundTasks,
+        session: AsyncSession = Depends(get_async_session),
+) -> BaseUserInfo:
     user = await get_user_by_email(new_user.email, session)
     if user:
         raise HTTPException(status_code=status.HTTP_409_CONFLICT, detail="Already exists")
 
     created_user = await create_user_in_db(new_user.email, new_user.name, new_user.password, session)
-    await rabbitmq_broker.send_message({"ss": "bb"}, SupportedQueues.USER_REGISTRATION)
+    background_task.add_task(
+        rabbitmq_broker.send_message,
+        message={
+            "name": created_user.name,
+            "email": created_user.email,
+            'redirect_url': str(request.url_for('verify_user', user_uuid=created_user.uuid_data))
+        },
+        queue_name=SupportedQueues.USER_REGISTRATION
+    )
+
     return created_user
 
 
