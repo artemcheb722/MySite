52613ad64912406114f51e1d6c079d157ae9df2b
Artem <artem.chebanyuk@gmail.com@email.com>
2025-06-17 20:22:38 +0300

    migrations

---
+++ backend_api/app/applications/products/router.py
@@ -0,0 +1,19 @@
+from fastapi import APIRouter
+
+
+products_router = APIRouter()
+
+@products_router.post('/')
+async def create_product():
+    pass
+
+
+@products_router.get('/{pk}')
+async def get_product(pk: int):
+    pass
+
+
+@products_router.get('/')
+async def get_products():
+    pass
+
