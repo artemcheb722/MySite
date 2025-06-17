27652106daf3b66c79574bc596e42fcaca50b5d4
Artem <artem.chebanyuk@gmail.com@email.com>
2025-06-17 20:41:34 +0300

    S3

--- backend_api/app/applications/products/router.py
+++ backend_api/app/applications/products/router.py
@@ -1,19 +1,14 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, Body, UploadFile
 
 
 products_router = APIRouter()
 
 @products_router.post('/')
-async def create_product():
+async def create_product(
+    main_image: UploadFile,
+    images: list[UploadFile],
+    title: str = Body(max_length=100),
+    description: str = Body(max_length=1000),
+    price: float = Body(gt=1),
+):
     pass
-
-
-@products_router.get('/{pk}')
-async def get_product(pk: int):
-    pass
-
-
-@products_router.get('/')
-async def get_products():
-    pass
-
